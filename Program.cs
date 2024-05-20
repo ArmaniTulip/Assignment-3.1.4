95a18745aa57eb36066174205601d71eb44d70f5
Garrett <Garrett@YUP>
2024-05-20 16:36:31 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Assignment_3._1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the value for X coordinate: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input the value for Y coordinate: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            if (x > 0 && y > 0)
+                Console.WriteLine($"The coordinate point ({x},{y}) lies in the First quadrant.");
+            else if (x < 0 && y > 0)
+                Console.WriteLine($"The coordinate point ({x},{y}) lies in the Second quadrant.");
+            else if (x < 0 && y < 0)
+                Console.WriteLine($"The coordinate point ({x},{y}) lies in the Third quadrant.");
+            else if (x > 0 && y < 0)
+                Console.WriteLine($"The coordinate point ({x},{y}) lies in the Fourth quadrant.");
+            else if (x == 0 && y == 0)
+                Console.WriteLine($"The coordinate point ({x},{y}) lies at the origin.");
+            else
+                Console.WriteLine($"The coordinate point ({x},{y}) lies on the axis.");
+
+
+
+
+
+        }
+
+    }
+}
+
+
+
+
+
+
+        
+    
+    
+
+
